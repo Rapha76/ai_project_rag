b3d7a8751511b7804d69c62d8f750c7d4063c126
Raphael B <raph.bandeira@gmail.com>
2026-04-23 21:33:29 +0200

    feat : segmentation et ingestion dans chromadb

--- scripts/ingest.py
+++ scripts/ingest.py
@@ -1,8 +1,12 @@
 from docling.document_converter import DocumentConverter
 from pathlib import Path
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+
 
 racine_projet = Path(__file__).parent.parent
-source = racine_projet / "data" / "doc1.pdf"
+source = racine_projet / "data" / "pdf-exemple.pdf"
 
 if not source.exists():
     print(f"Impossible de trouver le fichier à cet endroit {source}")
@@ -11,6 +15,21 @@ else :
     try : 
         converter = DocumentConverter()
         doc = converter.convert(source).document
-        print(doc.export_to_markdown())
+        text_markdown = doc.export_to_markdown()
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 1000,
+            chunk_overlap = 200
+        )
+        chunks = text_splitter.split_text(text_markdown)
+
+        embedding_model = OllamaEmbeddings(model="llama3")
+        vector_chroma = Chroma.from_texts(
+            texts = chunks,
+            embedding= embedding_model,
+            persist_directory= str(racine_projet / "chroma_db")
+        )
+
+        print(f"{len(chunks)} morceaux de texte sauvegardé dans la base vectorielle.")
+
     except Exception as e : 
         print(f"erreur pendant la conversion {e}")
